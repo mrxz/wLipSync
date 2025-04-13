ddf5f8193b8b1dda2534cc687ffc923d445a66fe
Noeri Huisman <8823461+mrxz@users.noreply.github.com>
2025-04-13 18:47:40 +0200

    Optimize memory copying done in copy_ring_buffer

--- src/algo/copy_ring_buffer.c
+++ src/algo/copy_ring_buffer.c
@@ -1,8 +1,15 @@
+#include "../math.h"
+
 void copy_ring_buffer(float *outputBuffer, float *inputBuffer,
                       unsigned long startIndex, unsigned long inputBufferSize,
                       unsigned long outputBufferSize) {
   startIndex %= inputBufferSize;
-  for(int i = 0; i < outputBufferSize; i++) {
-    outputBuffer[i] = inputBuffer[(startIndex + i) % inputBufferSize];
-  }
+
+  // Copy from the tail end of the inputBuffer (startIndex);
+  int tail = min(inputBufferSize - startIndex, outputBufferSize);
+  __builtin_memcpy(outputBuffer, &inputBuffer[startIndex], sizeof(float)*tail);
+
+  // Copy from the start of the inputBuffer (index 0)
+  int left = outputBufferSize - tail;
+  __builtin_memcpy(&outputBuffer[outputBufferSize - left], inputBuffer, sizeof(float)*left);
 }
